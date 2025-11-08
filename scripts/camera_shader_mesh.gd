6492f57e07edfe35cb7aa270ee6e156a050aab3b
Lasse Letager Hansen <letager@cs.au.dk>
2025-11-08 15:39:55 +0100

    Shader and sound for stress effect

---
+++ scripts/camera_shader_mesh.gd
@@ -0,0 +1,11 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mat = get_surface_override_material(0)
+	mat.set_shader_parameter("stress_level_1", get_node("../Player").is_stressed_1())
